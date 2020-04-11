892215a7150b59f054271c334c5423c067221a31
vladimir skipic <skipic@gmail.com>
2020-04-11 17:18:36 +0200

    Samurai App EntotyFrameworkCore PluralSight course

---
+++ SamuraiApp/SamuaraiApp.Domain/Clan.cs
@@ -0,0 +1,9 @@
+﻿namespace SamuaraiApp.Domain
+{
+    public class Clan
+    {
+        public int Id { get; set; }
+        public string ClanName { get; set; }
+
+    }
+}
---
+++ SamuraiApp/SamuaraiApp.Domain/Samurai.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Mail;
+
+namespace SamuaraiApp.Domain
+{
+    public class Samurai
+    {
+        public Samurai()
+        {
+            Quotes = new List<Quote>();
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Quote> Quotes { get; set; }
+        public Clan Clan { get; set; }
+    }
+}
